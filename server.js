90e26156f5573fe830189cea45228762719524d3
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-06 18:56:25 +0530

    route implemented for testing

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
+const messageRoutes = require("./routes/message");
+
 // app instance
 const app = express();
 
@@ -29,9 +31,11 @@ app.use(cors());
 const PORT = process.env.PORT || 2004;
 
 // endpoint for testing
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+//using endpoints
+app.use("/api", messageRoutes);
 
 // listen
 app.listen(PORT, () => {
