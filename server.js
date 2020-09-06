b09aa579f93333a7db1fe17eb246bdc69315511f
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-06 18:29:25 +0530

    db connection

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const cors = require("cors");
 // app instance
 const app = express();
 
+// DB config
+const databaseURL = process.env.DATABASE_URL;
+
+mongoose
+  .connect(databaseURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DATABASE CONNECTED");
+  })
+  .catch((error) => console.log(error));
+
 // middlewares
 app.use(express.json());
 app.use(cors());
