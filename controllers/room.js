feb314a198f726f3446858ae2042823936e12972
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-07 14:57:25 +0530

    controller for creating room

--- controllers/room.js
+++ controllers/room.js
@@ -1,2 +1,14 @@
-exports.createRoom = (req, res) => {};
+const Room = require("../models/room");
+
+exports.createRoom = (req, res) => {
+  const room = req.body;
+
+  Room.create(room, (error, room) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(201).send(room);
+    }
+  });
+};
 exports.getRooms = (req, res) => {};
--- routes/room.js
+++ routes/room.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const { createRoom, getRooms } = require("../controllers/room");
 
-router.post("/room/new");
+router.post("/room/new", createRoom);
 router.get("/room/all", getRooms);
 
 module.exports = router;
