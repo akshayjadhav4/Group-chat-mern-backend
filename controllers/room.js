5ed305594734058696b3e91a2641d21205f78074
AKSHAY <akshayjadhav461999@gmail.com>
2020-09-07 15:30:13 +0530

    realtime sync added for room

--- controllers/room.js
+++ controllers/room.js
@@ -1,4 +1,38 @@
 const Room = require("../models/room");
+const mongoose = require("mongoose");
+const Pusher = require("pusher");
+
+// PUSHER config
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: process.env.PUSHER_CLUSTER,
+  encrypted: process.env.PUSHER_ENCRYPTED,
+});
+
+const db = mongoose.connection;
+db.once("open", () => {
+  // console.log("PUSHER");
+  // mongodb chage stream
+  const roomCollection = db.collection("rooms");
+  const changeStream = roomCollection.watch();
+
+  changeStream.on("change", (change) => {
+    // console.log(change);
+
+    // actual pusher working
+    if (change.operationType === "insert") {
+      const roomDetails = change.fullDocument;
+      pusher.trigger("rooms", "inserted", {
+        _id: roomDetails._id,
+        name: roomDetails.name,
+      });
+    } else {
+      console.log("PUSHER ERROR");
+    }
+  });
+});
 
 exports.createRoom = (req, res) => {
   const room = req.body;
